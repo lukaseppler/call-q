b37419362aa0f4adb3b17d7547e43f49a05d48f7
Adrian Perez <adrianperez.deb@gmail.com>
2011-01-10 19:00:51 -0500

    Add project layout and setup

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'haml'
+gem 'mongoid', '2.0.0.rc.3'
+
+gem 'bson_ext', '1.1.5'
+
+group :development do
+  gem 'compass'
+end
+
+group :development, :test do
+  gem 'autotest'
+  gem 'capybara', '0.3.9'
+  gem 'cucumber', '>= 0.9.4'
+  gem 'cucumber-rails', '>= 0.3.0'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'rspec', '>= 2.4.0'
+  gem 'rspec-rails', '>= 2.4.1'
+  gem 'simplecov', :require => false
+  gem 'spork'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CallQ::Application.config.secret_token = '753350b240e3d66f195b8100e285fdea20396b62aa8eebf2f822f39d97cfeb6ae647047e1be53b9b60f714a2ed0d2c4d684658880211d678eea86a9583f3fd35'
