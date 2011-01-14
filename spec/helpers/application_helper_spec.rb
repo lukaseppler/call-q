50d977df60f4237df80fccbc316ec671921226b1
Adrian Perez <adrianperez.deb@gmail.com>
2011-01-13 22:30:39 -0500

    Rename helper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,7 +4,7 @@ describe ApplicationHelper do
   describe "#formatted_phone" do
     it "returns an international formatted phone" do
       phone = helper.formatted_phone "4111111111"
-      phone.should == '+41 11 111 11 1"
+      phone.should == "+41 11 111 11 1"
     end
   end
 end
